3a77d4795b67c39dbf14d2b759be9d405378181b
haozhang <haozhan@microsoft.com>
2025-03-11 14:50:41 +0800

    add default dockerfile for docker build for Java projects

--- cli/azd/pkg/project/framework_service_docker.go
+++ cli/azd/pkg/project/framework_service_docker.go
@@ -14,6 +14,8 @@ import (
 	"path/filepath"
 	"strings"
 
+	"go.opentelemetry.io/otel/trace"
+
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/azure/azure-dev/cli/azd/internal"
 	"github.com/azure/azure-dev/cli/azd/internal/appdetect"
@@ -30,8 +32,8 @@ import (
 	"github.com/azure/azure-dev/cli/azd/pkg/output"
 	"github.com/azure/azure-dev/cli/azd/pkg/tools"
 	"github.com/azure/azure-dev/cli/azd/pkg/tools/docker"
+	"github.com/azure/azure-dev/cli/azd/pkg/tools/maven"
 	"github.com/azure/azure-dev/cli/azd/pkg/tools/pack"
-	"go.opentelemetry.io/otel/trace"
 )
 
 type DockerProjectOptions struct {
@@ -198,6 +200,22 @@ func (p *dockerProject) Build(
 ) (*ServiceBuildResult, error) {
 	if serviceConfig.Docker.RemoteBuild || useDotnetPublishForDockerBuild(serviceConfig) {
 		return &ServiceBuildResult{Restore: restoreOutput}, nil
+	}
+
+	// If it's a java project without Dockerfile, we help to package jar and add a default Dockerfile for Docker build
+	if serviceConfig.Language == ServiceLanguageJava && serviceConfig.Docker.Path == "" {
+		mvnCli := maven.NewCli(exec.NewCommandRunner(nil))
+		err := mvnCli.CleanPackage(ctx, serviceConfig.RelativePath, serviceConfig.Project.Path)
+		if err != nil {
+			return nil, err
+		}
+		defaultDockerfilePath, err := addDefaultDockerfileForJavaProject(serviceConfig.Name)
+		if err != nil {
+			return nil, err
+		}
+		serviceConfig.Docker = DockerProjectOptions{
+			Path: defaultDockerfilePath,
+		}
 	}
 
 	dockerOptions := getDockerOptionsWithDefaults(serviceConfig.Docker)
@@ -607,4 +625,28 @@ func getDockerOptionsWithDefaults(options DockerProjectOptions) DockerProjectOpt
 	}
 
 	return options
+}
+
+const DefaultDockerfileForJavaProject = `FROM openjdk:21-jdk-slim
+COPY ./target/*.jar /app.jar
+ENTRYPOINT ["java", "-jar", "/app.jar"]`
+
+func addDefaultDockerfileForJavaProject(svcName string) (string, error) {
+	log.Printf("Dockerfile not found for java project %s, will provide a default one", svcName)
+	dockerfileDir, err := os.MkdirTemp("", svcName)
+	if err != nil {
+		return "", fmt.Errorf("error creating temp Dockerfile directory: %w", err)
+	}
+
+	dockerfilePath := filepath.Join(dockerfileDir, "Dockerfile")
+	file, err := os.Create(dockerfilePath)
+	if err != nil {
+		return "", fmt.Errorf("error creating Dockerfile at %s: %w", dockerfilePath, err)
+	}
+	defer file.Close()
+
+	if _, err = file.WriteString(DefaultDockerfileForJavaProject); err != nil {
+		return "", fmt.Errorf("error writing Dockerfile at %s: %w", dockerfilePath, err)
+	}
+	return dockerfilePath, nil
 }
